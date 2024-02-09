880fd8fc92c6f3eb603730543dd5b57232ab5060
douattar <douattar@pixel.home>
2024-02-09 14:40:59 +0100

    add employee.py : move the employee class here

---
+++ srcs/employee.py
@@ -0,0 +1,44 @@
+from dataclasses import dataclass, asdict, field
+
+@dataclass
+class Employee:
+    name: str = None
+    phone: str = None
+    email: str = None
+    website: str = None
+
+@dataclass
+class EmployeeList:
+    employee_list: list[Employee] = field(default_factory=list)
+    save_at = 'output'
+
+    def dataframe(self):
+        """transform business_list to pandas dataframe
+
+        Returns: pandas dataframe
+        """
+        return pd.json_normalize(
+            (asdict(business) for business in self.business_list), sep="_"
+        )
+
+    def save_to_excel(self, filename):
+        """saves pandas dataframe to excel (xlsx) file
+
+        Args:
+            filename (str): filename
+        """
+
+        if not os.path.exists(self.save_at):
+            os.makedirs(self.save_at)
+        self.dataframe().to_excel(f"output/{filename}.xlsx", index=False)
+
+    def save_to_csv(self, filename):
+        """saves pandas dataframe to csv file
+
+        Args:
+            filename (str): filename
+        """
+
+        if not os.path.exists(self.save_at):
+            os.makedirs(self.save_at)
+        self.dataframe().to_csv(f"output/{filename}.csv", index=False)
