7d92ab8bdc4964f1918c3866450a5b0ccf9b3ed9
mariomario <yo@example.com>
2024-02-09 19:39:34 +0100

    less waiting time

--- srcs/run.py
+++ srcs/run.py
@@ -22,29 +22,74 @@ def main():
     # scraping
     ###########
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
+        browser = p.chromium.launch(headless=True)
         page = browser.new_page()
 
         page.goto(args.url, timeout=60000)
         # wait is added for dev phase. can remove it in production
-        page.wait_for_timeout(5000)
+        page.wait_for_timeout(500)
 
         business_list = BusinessList()
 
         next = page.locator(next_xpath)
-        while next.count() > 0:
+        i : int = 0
+        while next.count() > 0 or i < 1:
+            if (next.count() == 0):
+                i += 1
             try:
 
                 for office in page.locator(offices_xpath).all():
                     office.locator(office_page_xpath).click()
-                    page.wait_for_timeout(5000)
+                    page.wait_for_timeout(500)
+
                     business = Business()
-                    
+                    if page.locator(name_xpath).count() > 0:
+                        business.name = page.locator(name_xpath).all()[0].inner_text()
+                    else:
+                        business.name = ""
+                    print(business.name)
+                    if page.locator(address_xpath).count() > 0:
+                        business.address = page.locator(address_xpath).all()[0].inner_text()
+                    else:
+                        business.address = ""
+                    if page.locator(postal_code_xpath).count() > 0:
+                        business.postal_code = page.locator(postal_code_xpath).all()[0].inner_text()
+                    else:
+                        business.postal_code = ""
+                    if page.locator(locality_xpath).count() > 0:
+                        business.locality = page.locator(locality_xpath).all()[0].inner_text()
+                    else:
+                        business.locality = ""
+                    if page.locator(locality_xpath).count() > 0:
+                        business.locality = page.locator(locality_xpath).all()[0].inner_text()
+                    else:
+                        business.locality = ""
+                    if page.locator(country_xpath).count() > 0:
+                        business.country = page.locator(country_xpath).all()[0].inner_text()
+                    else:
+                        business.country = ""
+
+                    if page.locator(website_xpath).count() > 0:
+                        business.website = page.locator(website_xpath).all()[0].inner_text()
+                    else:
+                        business.website = ""
+                    if page.locator(language_xpath).count() > 0:
+                        business.language = page.locator(language_xpath).all()[0].inner_text()
+                    else:
+                        business.language = ""
+                    if page.locator(email_xpath).count() > 0:
+                        business.email = page.locator(email_xpath).all()[0].get_attribute('href')[7:]
+                    else:
+                        business.email = ""
+                    if page.locator(phone_xpath).count() > 0:
+                        business.phone = page.locator(phone_xpath).all()[0].inner_text()
+                    else:
+                        business.phone = ""
                     business_list.business_list.append(business)
                     page.go_back()
-                    pass
-                next.click()
-                page.wait_for_timeout(5000)
+                if (next.count() > 0):
+                    next.click()
+                    page.wait_for_timeout(500)
                 next = page.locator(next_xpath)
             except Exception as e:
                 print(f'Error occured: {e}')
@@ -52,8 +97,8 @@ def main():
         #########
         # output
         #########
-        business_list.save_to_excel(f"notaire_{search_for}".replace(' ', '_'))
-        business_list.save_to_csv(f"notaire_{search_for}".replace(' ', '_'))
+        business_list.save_to_excel(f"notaire_paris".replace(' ', '_'))
+        business_list.save_to_csv(f"notaire_paris".replace(' ', '_'))
 
         browser.close()
 
