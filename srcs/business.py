1d246fa845a95692ccef375a026f6a8b65dbae4a
douattar <douattar@pixel.home>
2024-02-09 14:40:41 +0100

    add business.py : move the business class here

---
+++ srcs/business.py
@@ -0,0 +1,56 @@
+''' This file contains the class business with the stuff to write in it'''
+from dataclasses import dataclass, asdict, field
+
+@dataclass
+class Business:
+    name: str = None
+
+    # Localisation
+    address: str = None
+    postal_code : int = None
+    locality : str = None
+    country : str = None
+
+    # Contact
+    website: str = None
+    language: str = None
+    email: str = None
+
+@dataclass
+class BusinessList:
+    """holds list of Business objects,
+    and save to both excel and csv
+    """
+    business_list: list[Business] = field(default_factory=list)
+    save_at = 'output'
+
+    def dataframe(self):
+        """transform business_list to pandas dataframe
+
+        Returns: pandas dataframe
+        """
+        return pd.json_normalize(
+            (asdict(business) for business in self.business_list), sep="_"
+        )
+
+    def save_to_excel(self, filename):
+        """saves pandas dataframe to excel (xlsx) file
+
+        Args:
+            filename (str): filename
+        """
+
+        if not os.path.exists(self.save_at):
+            os.makedirs(self.save_at)
+        self.dataframe().to_excel(f"output/{filename}.xlsx", index=False)
+
+    def save_to_csv(self, filename):
+        """saves pandas dataframe to csv file
+
+        Args:
+            filename (str): filename
+        """
+
+        if not os.path.exists(self.save_at):
+            os.makedirs(self.save_at)
+        self.dataframe().to_csv(f"output/{filename}.csv", index=False)
